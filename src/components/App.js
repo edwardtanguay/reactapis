da3283f6b41280ba65f69a5f2d2d18c0fdfab941
Edward Tanguay (hm.win) <edward@tanguay.info>
2020-06-19 06:18:31 +0200

    generic feed

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import News from './News/News';
 
-function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<h1>Welcome</h1>
-			</header>
-			<News/>
-		</div>
-	);
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			news1: {
+				query: 'paris',
+				language: 'fr'
+			},
+			news2: {
+				query: 'berlin',
+				language: 'de'
+			}
+		};
+	}
+	render() {
+		return (
+			<div className="App">
+				<News news={this.state.news1} />
+				<News news={this.state.news2} />
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/components/News/News.js
+++ src/components/News/News.js
@@ -11,24 +11,34 @@ class News extends Component {
 	}
 
 	renderItems() {
-		return this.props.items.map(item => (
-			<NewSingle key={item.id} item={item} />
+		return this.state.news.map(item => (
+			<NewSingle key={item.url} item={item} />
 		));
 	}
 
 	componentDidMount() {
-		const url = 'http://newsapi.org/v2/everything?q=bitcoin&from=2020-05-18&sortBy=publishedAt&apiKey=34c534a3b60d46ed81a257c952fbb3da';
-		fetch(url) 
+		const url = `http://newsapi.org/v2/everything?q=${this.props.news.query}&pageSize=100&language=${this.props.news.language}&sortBy=publishedAt&apiKey=34c534a3b60d46ed81a257c952fbb3da`;
+		//const url = 'http://newsapi.org/v2/everything?q=marseille&pageSize=100&language=fr&from=2020-05-18&sortBy=publishedAt&apiKey=34c534a3b60d46ed81a257c952fbb3da';
+		//const url = 'http://newsapi.org/v2/top-headlines?country=fr&apiKey=34c534a3b60d46ed81a257c952fbb3da';
+		//https://newsapi.org/v2/everything?q=bitcoin&apiKey=34c534a3b60d46ed81a257c952fbb3da
+		fetch(url)
 			.then(response => {
-			
-		})
+				return response.json();
+			})
+			.then(data => {
+				this.setState({
+					news: data.articles,
+					totalResults: data.totalResults
+				})
+			}).catch(error => console.log(error))
 	}
 
 	render() {
 		return (
-			<ul>
+			<div>
+				<div className="title">{this.props.news.query} - {this.props.news.language}</div>
 				{this.renderItems()}
-			</ul>
+			</div>
 		);
 	}
 }
